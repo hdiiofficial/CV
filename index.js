6989eefce7ff9928289c9c284ddaec3788fd7683
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2023-07-06 14:12:36 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,385 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+  <title>My Curriculum vitae</title>
+
+  <style>
+
+    body {
+
+      font-family: Arial, sans-serif;
+
+      background-color: #f5f5f5;
+
+      padding: 20px;
+
+    }
+
+
+
+    h1 {
+
+      text-align: center;
+
+      margin-bottom: 20px;
+
+      color: #333333;
+
+      text-transform: uppercase;
+
+      letter-spacing: 2px;
+
+    }
+
+
+
+    .section {
+
+      margin-bottom: 30px;
+
+      background-color: #ffffff;
+
+      padding: 20px;
+
+      border-radius: 5px;
+
+      box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+
+    }
+
+
+
+    .section h2 {
+
+      margin-bottom: 10px;
+
+      color: #333333;
+
+    }
+
+
+
+    .section ul {
+
+      list-style-type: none;
+
+      padding-left: 0;
+
+      margin-top: 0;
+
+    }
+
+
+
+    .section li {
+
+      margin-bottom: 5px;
+
+      color: #555555;
+
+    }
+
+
+
+    .section .title {
+
+      font-weight: bold;
+
+      color: #333333;
+
+    }
+
+
+
+    .section table {
+
+      width: 100%;
+
+      border-collapse: collapse;
+
+    }
+
+
+
+    .section table th,
+
+    .section table td {
+
+      padding: 8px;
+
+      text-align: left;
+
+      border-bottom: 1px solid #dddddd;
+
+    }
+
+
+
+    .section table th {
+
+      background-color: #f5f5f5;
+
+    }
+
+
+
+    .section table td.skills {
+
+      display: flex;
+
+    }
+
+
+
+    .section table td.skills span {
+
+      background-color: #f5f5f5;
+
+      border-radius: 3px;
+
+      color: #555555;
+
+      font-size: 14px;
+
+      padding: 4px 8px;
+
+      margin-right: 5px;
+
+    }
+
+
+
+    img.profile-pic {
+
+      display: block;
+
+      margin: 0 auto;
+
+      width: 150px;
+
+      height: 150px;
+
+      object-fit: cover;
+
+      border-radius: 50%;
+
+      box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+
+    }
+
+
+
+    .skills-bar {
+
+      background-color: #eeeeee;
+
+      height: 10px;
+
+      border-radius: 5px;
+
+      margin-top: 5px;
+
+      overflow: hidden;
+
+    }
+
+
+
+    .skills-progress {
+
+      height: 100%;
+
+      background-color: #4caf50;
+
+    }
+
+
+
+    .linkedin-link {
+
+      color: #0077b5;
+
+      text-decoration: none;
+
+    }
+
+
+
+    .linkedin-link:hover {
+
+      text-decoration: underline;
+
+    }
+
+  </style>
+
+</head>
+
+<body>
+
+  <h1>Curriculum vitae</h1>
+
+
+
+  <div class="section" style="background-color: #f7f7f7;">
+
+    <h2 style="color: #333333;">Personal Information</h2>
+
+    <img class="profile-pic" src="https://telegra.ph/file/4689293c16315ba293bf0.jpg" alt="Profile Picture">
+
+    <ul>
+
+      <li><span class="title">Name:</span> Suseno Hadi Maksum</li>
+
+      <li><span class="title">Email:</span> hdiiofficialfeedback@gmail.com</li>
+
+      <li><span class="title">Phone:</span> +62 857-0092-1759</li>
+
+      <li><span class="title">Address:</span> Campuranom, Bansari, Temanggung, Jawa Tengah</li>
+
+      <li><span class="title">Personal Web:</span> <a class="linkedin-link" href="https://hdiiofficial.github.io" target="_blank">HADI-DEV.dev.id</a></li>
+
+    </ul>
+
+  </div>
+
+
+
+  <div class="section" style="background-color: #f7f7f7;">
+
+    <h2 style="color: #333333;">Education</h2>
+
+    <table>
+
+      <tr>
+
+        <th>Degree</th>
+
+        <th>University</th>
+
+        <th>Year</th>
+
+      </tr>
+
+      <tr>
+
+        <td>Associate's degree, Electrical Engineering</td>
+
+        <td>Pakistani Swedish Institute of Technology</td>
+
+        <td>2021 - 2024</td>
+
+      </tr>
+
+      <tr>
+
+        <td>Bachelor of Science in Computer Science</td>
+
+        <td>NED University of Technalogy</td>
+
+        <td>2023 - 2027</td>
+
+      </tr>
+
+    </table>
+
+  </div>
+
+
+
+  <div class="section" style="background-color: #f7f7f7;">
+
+    <h2 style="color: #333333;">Work Experience</h2>
+
+    <table>
+
+      <tr>
+
+        <th>Position</th>
+
+        <th>Company</th>
+
+        <th>Dates</th>
+
+      </tr>
+
+      <tr>
+
+        <td>Founder</td>
+
+        <td>Indollar</td>
+
+        <td>May 2021 - Present</td>
+
+      </tr>
+
+      <tr>
+
+        <td>Co-founder</td>
+
+        <td>Skinned Orbital LTD</td>
+
+        <td>Feb 2023 - Present</td>
+
+      </tr>
+
+    </table>
+
+  </div>
+
+
+
+  <div class="section" style="background-color: #f7f7f7;">
+
+    <h2 style="color: #333333;">Skills</h2>
+
+    <table>
+
+      <tr>
+
+        <td class="skills">
+
+          <span>Python</span>
+
+          <span>AI/ML</span>
+
+          <span>Power BI</span>
+
+        </td>
+
+      </tr>
+
+    </table>
+
+    <div class="skills-bar">
+
+      <div class="skills-progress" style="width: 80%;"></div>
+
+    </div>
+
+  </div>
+
+
+
+  <div class="section" style="background-color: #f7f7f7;">
+
+    <h2 style="color: #333333;">Courses and Certifications</h2>
+
+    <ul>
+
+      <li>Crash Course on Python (Google)</li>
+
+      <li>AI for Everyone (Deep Learning.AI)</li>
+
+      <li>Microsoft Power Platform Fundamentals (Microsoft)</li>
+
+    </ul>
+
+  </div>
+
+</body>
+
+</html>
